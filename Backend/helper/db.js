7467801b807c95c08fcb05717d1c45e9f5e2ac99
Joonatan Varkki <t3vajo05@students.oamk.fi>
2025-09-04 16:45:00 +0300

    Added db.js

---
+++ Backend/helper/db.js
@@ -0,0 +1,17 @@
+import pkg from 'pg'
+const { Pool } = pkg
+import dotenv from 'dotenv'
+
+const environment = process.env.NODE_ENV
+
+dotenv.config()
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+export { pool }
