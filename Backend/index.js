8a6f21f08fd6b80dbcb3b23cbc8de461d76e181a
Joonatan Varkki <t3vajo05@students.oamk.fi>
2025-09-04 17:09:22 +0300

    Created a sample /users route to output data from db

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { pool } from './helper/db.js'
 
 const port = 3001
 const app = express()
@@ -8,6 +9,18 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.status(200).json({result: "Success"})
+})
+
+app.get('/users', (req, res) => {
+    pool.query('SELECT * FROM users', (err, result) => {
+        console.log("Getting users")
+        if (err) {
+            console.error("Query error:", err)
+            return res.status(500).json({error: err.message})
+        }
+        console.log("Query result:", result.rows)
+        res.status(200).json(result.rows || [])
+        })
 })
 
 app.listen(port, () => {
