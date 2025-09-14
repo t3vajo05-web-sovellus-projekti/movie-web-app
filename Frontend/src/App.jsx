33225068da33376816d6b7d6c5a45066618a4a68
Juho Starck <jumpe27@gmail.com>
2025-09-14 13:06:02 +0300

    frontend muutokset ennen mergeä

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignUp from '../components/SignUp.jsx'
+import SignIn from '../components/SignIn.jsx'
+import DeleteUser from '../components/DeleteUser.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [token, setToken] = useState(localStorage.getItem('token') || null)
+  const [view, setView] = useState('signup')
+
+  const saveToken = (newToken) => {
+    if(newToken) {
+      localStorage.setItem('token', newToken)
+    } else {
+      localStorage.removeItem('token')
+    }
+    setToken(newToken)
+  }
+
+  const logout = () => {
+    saveToken(null)
+  }
 
   return (
-    <b>Hello world!</b>
+
+    <div>
+      {!token ? (
+        <>
+          <div>
+            <button onClick={() => setView('signin')}>Sign in</button>
+            <button onClick={() => setView('signup')}>Sign up</button>
+          </div>
+          {view === 'signin' && <SignIn setToken={saveToken} />}
+          {view === 'signup' && <SignUp />}
+        </>
+      ) : (
+        <>
+          <DeleteUser token={token} setToken={saveToken} />
+          <button onClick={logout}>Logout</button>
+        </>
+      )}
+    </div>
   )
 }
 
