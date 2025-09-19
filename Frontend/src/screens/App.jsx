f2e2ccbe9623e890245de9f6415257907237275c
Joonatan Varkki <t3vajo05@students.oamk.fi>
2025-09-19 20:25:35 +0300

    Alettu tekemään profiilisivua

--- Frontend/src/screens/App.jsx
+++ Frontend/src/screens/App.jsx
@@ -20,6 +20,7 @@ import Login from '../pages/Login.jsx'
 import Signup from '../pages/Signup.jsx'
 import Moviesearch from '../pages/Moviesearch.jsx'
 import UserProvider from '../context/userProvider.jsx'
+import MyProfile from '../pages/MyProfile.jsx'
 
 /*
 function App() {
@@ -66,6 +67,7 @@ function App() {
           <Route path="/signup" exact element={<Signup />} />
           <Route path="/login" exact element={<Login />} />
           <Route path="/moviesearch" exact element={<Moviesearch />} />
+          <Route path="/myprofile" exact element={<MyProfile />} />
 
           <Route path="/*" exact element={<NotFound />} />
         </Routes>
