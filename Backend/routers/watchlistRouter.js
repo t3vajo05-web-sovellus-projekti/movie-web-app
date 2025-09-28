05ff3285eb1581a633fb09a035462c3b79f38ca6
Joonatan Varkki <t3vajo05@students.oamk.fi>
2025-09-28 13:36:53 +0300

    Luotu public watchlist ominaisuus (/watchlist/user/:id)

--- Backend/routers/watchlistRouter.js
+++ Backend/routers/watchlistRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { auth } from '../helper/auth.js'
-import { getMyWatchlist, addWatchlistItem, deleteWatchlistItem, toggleFavoriteItem, setStatusItem } from '../controllers/watchlistController.js'
+import { getMyWatchlist, addWatchlistItem, deleteWatchlistItem, toggleFavoriteItem, setStatusItem, getUserWatchlist } from '../controllers/watchlistController.js'
 
 const router = Router()
 
 router.get('/', auth, getMyWatchlist)
+router.get('/user/:userId', getUserWatchlist)
 router.post('/:movieId', auth, addWatchlistItem)
 router.delete('/:movieId', auth, deleteWatchlistItem)
 router.patch('/item/:id/favorite', auth, toggleFavoriteItem)
